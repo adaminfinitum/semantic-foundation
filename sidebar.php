09d72421794b0cc98f6f7aae7e2a53771791dfe0
Andreas <alicht83@gmail.com>
2014-06-13 23:38:51 +0200

    Add WAI ARIA role="complementary" to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<aside id="sidebar" class="small-12 large-4 columns">
+<aside id="sidebar" class="small-12 large-4 columns" role="complementary">
 	<?php do_action('semantic_foundation_before_sidebar'); ?>
 	<?php dynamic_sidebar("sidebar-widgets"); ?>
 	<?php do_action('semantic_foundation_after_sidebar'); ?>
