d827d851c00ffaa83e9859a90dc611e978349257
Andreas <alicht83@gmail.com>
2014-06-16 00:10:13 +0200

    add fatfooter

--- footer.php
+++ footer.php
@@ -1,7 +1,18 @@
 			</section><!-- #content -->
 			<footer class="row">
 				<?php do_action('semantic_foundation_before_footer'); ?>
-				<?php dynamic_sidebar("footer-widgets"); ?>
+				<!-- the .fatfooter aside - I use this to enable a screen-wide background on the footer while still keeping the footer contents in line with the layout -->
+    		<div class="fatfooter" role="complementary">
+					<?php dynamic_sidebar("footer-widgets"); ?>
+				</div><!-- #fatfooter -->
+				<section class="colophon" role="contentinfo">
+					<small class="copyright half left">
+						© 2014
+					</small><!-- .copyright -->
+					<small class="credits half right">
+						Powered by <a href="http://wordpress.org/">WordPress</a>.
+					</small><!-- .credits -->
+				</section><!--.colophon-->
 				<?php do_action('semantic_foundation_after_footer'); ?>
 			</footer>
 			<a class="exit-off-canvas"></a>
